bec59705e2f2c217f2ca8431bcbb2fb25de435c5
Bobby Martin <bobbymartin2@gmail.com>
2023-04-16 16:15:26 -0700

    Add summarization feature to agent

--- agent.py
+++ agent.py
@@ -1,36 +1,94 @@
 #!/usr/bin/python3
 
 import openai
+import re
+# Feel free to remove this if it doesn't resolve; see openai.api_key below
+import pii
 
 # Set the API key
-openai.api_key = "YOUR_OPENAI_API_KEY"
+# You can drop 'import pii' and replace this with 'YOUR_OPENAI_API_KEY'
+openai.api_key = pii.openai_api_key
+
+#MAX_TOKENS_BEFORE_SUMMARY=110
+#MAX_TOKENS_AFTER_SUMMARY=80
+MAX_TOKENS_BEFORE_SUMMARY=3000
+MAX_TOKENS_AFTER_SUMMARY=2000
 
 # Maintain a threaded conversation with ChatGPT, sending one message at a time
 # and getting a response that takes into account the conversation so far.
 class ChatConversation:
-  def __init__(self, agentDescription, modelEngine = "gpt-3.5-turbo", temperature=0.7):
-      self.agentDescription = agentDescription
-      self.modelEngine = modelEngine
-      self.temperature = temperature
-      self.messages = [{"role": "system", "content": agentDescription}]
+  def __init__(self, agentDescription, modelEngine = 'gpt-3.5-turbo', temperature=0.7):
+    self.agentDescription = agentDescription
+    self.modelEngine = modelEngine
+    self.temperature = temperature
+    self.messages = [{'role': 'system', 'content': agentDescription}]
+    self.totalTokens = 0
+
+  def asMessage(self, m):
+    if m['role']:
+      return m
+    else:
+      return m['choices'][0]['message']
 
   def chat(self, request):
-    self.messages.append({"role": "user", "content": request})
+    self.summarizeAsNeeded()
+
+    self.messages.append({'role': 'user', 'content': request})
     completion = openai.ChatCompletion.create(
-            messages = self.messages,
+            messages = [self.asMessage(m) for m in self.messages],
             model=self.modelEngine,
             temperature=self.temperature)
+    self.messages.append(completion)
+    self.totalTokens = completion['usage']['total_tokens']
 
     replyObj = completion['choices'][0]['message']
     reply = None
     if replyObj:
       reply = replyObj['content']
-      self.messages.append(replyObj)
+      completion['content'] = reply
+      completion['role'] = replyObj['role']
 
     return reply
 
+  def summarizeAsNeeded(self):
+    if self.totalTokens > MAX_TOKENS_BEFORE_SUMMARY:
+      summary = self.summarize()
+      self.messages = [{'role': 'system', 'content': self.agentDescription}]
+      self.messages.append({'role': 'assistant', 'content': summary})
+
+  def convo(self):
+      return self.messagesToString(self.messages)
+
+  def messagesToString(self, messages):
+      return "\n\n".join([m['role'] + " said: \"" + m['content'] + "\"\n" for m in messages if m['role'] != 'system'])
+
+  def summarize(self):
+    prevMessages = self.convo()
+    messages = [{'role': 'system', 'content': "You write Cliff's Notes for LLMs, but with no concern for grammar, only brevity."}]
+    messages.append({
+        'role': 'user',
+        'content': "Provide an overview of the following conversation in fewer than "
+        + str(MAX_TOKENS_AFTER_SUMMARY) + " words.\n\n" + prevMessages}
+        )
+    completion = openai.ChatCompletion.create(
+            messages = messages,
+            model=self.modelEngine,
+            temperature=self.temperature)
+
+    replyObj = completion['choices'][0]['message']
+    return replyObj['content'] if replyObj else None
+
+  # Pretend you had an exchange with the agent
+  def replay(request, reply):
+    self.messages.append({'role': 'user', 'content': request})
+    self.messages.append({'role': 'assistant', 'content': reply})
 
 def example():
   agent=ChatConversation("You are a master of linguistics and children's literature.")
   print(agent.chat("Where in the world is Carmen Lasvegas?"))
   print(agent.chat("I asked about Carmen Lasvegas, not Carmen Sandiego."))
+  print(agent.chat("Can you invent a story about Carmen Lasvegas?"))
+  print("\n\nAgent after convo: " + agent.convo())
+
+if __name__ == "__main__":
+    example()
