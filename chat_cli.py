880ad1b62d61629ff526e368a16831e7da77101f
Bobby Martin <bobbymartin2@gmail.com>
2023-04-16 21:20:58 -0700

    Support ongoing conversations (stored in files) and improve summarization

--- chat_cli.py
+++ chat_cli.py
@@ -17,36 +17,72 @@ def parseArgs():
   return parser.parse_args()
 
 class Convo:
-  def __init__(self, system, identifier = None):
+  def __init__(self, system = "You are a helpful assistant.", identifier = None):
     self.system = system
-    self.identifier = identifier
-    self.temp_dir = tempfile.mkdtemp(dir='local')
-    self._writeTempFile("system", json.dumps(self.system))
-    self.idx = 0
+    self.agent = None
+    if identifier:
+      self.temp_dir = pathlib.Path('local').joinpath(identifier)
+      if os.path.isdir(self.temp_dir):
+        self.loadConvo()
+      else:
+        os.mkdir(self.temp_dir)
+        self._writeTempFile("system", json.dumps(self.system))
+        self.idx = 0
+    else: 
+      self.temp_dir = tempfile.mkdtemp(dir='local')
+      self._writeTempFile("system", json.dumps(self.system))
+      self.idx = 0
 
   def _writeTempFile(self, filename, contents):
     with open(pathlib.Path(self.temp_dir).joinpath(filename), 'w') as f:
       f.write(contents)
 
+  def _readTempFile(self, filename):
+    fpath = pathlib.Path(self.temp_dir).joinpath(filename)
+    if os.path.isfile(fpath):
+      with open(fpath, 'r') as f:
+        c = f.read()
+        return json.loads(c)
+    else:
+      return None
+
   def id(self):
     return os.path.basename(self.temp_dir)
 
   def createChatConversation(self):
-    return chat_conversation.ChatConversation(self.system)
+    if not self.agent:
+      self.agent = chat_conversation.ChatConversation(self.system)
+    return self.agent
 
   def addExchange(self, exchange):
     self._writeTempFile(str(self.idx), json.dumps(exchange))
 
-def loadConvo(convoId):
-  #TODO actually load the conversation from the directory
-  return Convo("You are a helpful assistant.")
+  def loadConvo(self):
+      system = self._readTempFile("system")
+      if system: self.system = system
+
+      self.createChatConversation()
+
+      self.idx = 0
+      while True:
+        arr = self._readTempFile(str(self.idx))
+        if arr:
+          self.agent.replayObj(arr[0], arr[1])
+          self.idx = self.idx + 1
+        else:
+          break
 
 def main():
   args = parseArgs()
   if args.convo:
-    convo = loadConvo(args.convo)
+    convo = Convo(identifier=args.convo)
+    agent = convo.createChatConversation()
+    latestExchange = agent.getLatestExchange()
+    if latestExchange and len(latestExchange) > 0:
+      print("You last said to me: " + agent.asMessage(latestExchange[0])['content'])
+      print("and I replied: " + agent.asMessage(latestExchange[1])['content'])
   else:
-    system = args.role if args.role else "You are a helpful assistant."
+    system = args.role
     convo = Convo(system) 
     print("Conversation id: " + convo.id())
 
--- chat_conversation.py
+++ chat_conversation.py
@@ -62,6 +62,7 @@ class ChatConversation:
       summary = self.summarize()
       self.messages = [{'role': 'system', 'content': self.agentDescription}]
       self.messages.append({'role': 'assistant', 'content': summary})
+      print("Summary: " + summary)
 
   def convo(self):
       return self.messagesToString(self.messages)
@@ -75,8 +76,8 @@ class ChatConversation:
     messages = [{'role': 'system', 'content': "You write Cliff's Notes for LLMs, but with no concern for grammar, only brevity."}]
     messages.append({
         'role': 'user',
-        'content': "Provide an overview of the following conversation in fewer than "
-        + str(MAX_TOKENS_AFTER_SUMMARY) + " words.\n\n" + prevMessages}
+        'content': "Provide an overview of the following conversation in as many as "
+        + str(MAX_TOKENS_AFTER_SUMMARY) + " words. Include all important details, but you're just making notes for yourself; they can be quite cryptic as long as you'll understand them.\n\n" + prevMessages}
         )
     completion = openai.ChatCompletion.create(
             messages = messages,
@@ -87,19 +88,29 @@ class ChatConversation:
     return replyObj['content'] if replyObj else None
 
   # Pretend you had an exchange with the agent
+  def replayObj(self, request, reply):
+    self.messages.append(request)
+    self.messages.append(reply)
+    if 'usage' in reply:
+      self.totalTokens = reply['usage']['total_tokens']
+      self.summarizeAsNeeded()
+
+  # Pretend you had an exchange with the agent
   def replay(self, request, reply):
-    self.messages.append({'role': 'user', 'content': request})
-    self.messages.append({'role': 'assistant', 'content': reply})
+    self.replayObj(
+            {'role': 'user', 'content': request},
+            {'role': 'assistant', 'content': reply})
 
   def getLatestExchange(self):
-    return [self.messages[-2], self.messages[-1]]
+    if len(self.messages) > 1:
+      return [self.messages[-2], self.messages[-1]]
 
 def example():
   agent=ChatConversation("You are a master of linguistics and children's literature.")
   print(agent.chat("Where in the world is Carmen Lasvegas?"))
   print(agent.chat("I asked about Carmen Lasvegas, not Carmen Sandiego."))
   print(agent.chat("Can you invent a story about Carmen Lasvegas?"))
-  print("\n\nAgent after convo: " + agent.convo())
+  #print("\n\nAgent after convo: " + agent.convo())
 
 if __name__ == "__main__":
     example()
